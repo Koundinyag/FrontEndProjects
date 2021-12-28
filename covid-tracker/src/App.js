cfddd19e69bb05e1ac2ed64e3fb82626f0661620
Koundu-G <84827185+Koundinyag@users.noreply.github.com>
2021-12-28 18:28:53 +0530

    Step2: Default Application Modified for our application

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Default Application is setup for the Our Application
+      Default Application is modified for the Our Application
       </header>
     </div>
   );
