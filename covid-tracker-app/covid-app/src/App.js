625e13cc3e3be5c09e2cb45137fe6128b402be6e
Koundu-G <84827185+Koundinyag@users.noreply.github.com>
2022-01-13 14:16:12 +0530

    Final Covid Application

--- covid-tracker-app/covid-app/src/App.js
+++ covid-tracker-app/covid-app/src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import {Cards,Chart,CountryPicker} from './components/index'
+import React from 'react'
+import styles from './App.module.css'
+import {fetchData} from './api'
+import coronaImage from './images/covid-image.png'
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <br />
+        <text>
+          <b>Global and Country Wise Cases of Corona Virus</b>
+        </text>
+        <br />
+        <text>
+          <i>(For a particular select a Country from below)</i>
+        </text>
+        <br />
+        <br />
+        <Cards data={data} country={country} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
